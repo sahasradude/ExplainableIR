0e78329c686edcb280bdc9c1cc3782f49ad4327a
sahasradude <dhruvas@vt.edu>
2018-11-20 02:19:58 -0500

    added .idea files, changes to makeCorpus and predict_proba

--- explainable-results/src/java/makeCorpus.java
+++ explainable-results/src/java/makeCorpus.java
@@ -4,6 +4,8 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -18,27 +20,31 @@ public class makeCorpus {
         String idxpath = dirpath + "/index_lucene_robust04_krovetz";
         String field_docno = "docno";
         String field_search = "content";
-        Set<String> fieldset = new HashSet<>();
-        fieldset.add( "docno" );
         try {
+            FileWriter fw = new FileWriter("corpus_bow.txt");
+            Set<String> fieldset = new HashSet<>();
+            fieldset.add( "docno" );
+
             Directory dirLucene = FSDirectory.open((new File(idxpath)).toPath());
             IndexReader index = DirectoryReader.open(dirLucene);
 
             for(int docid = 0; docid < index.maxDoc(); docid++) {
                 String docno = index.document( docid, fieldset).get( "docno" );
+                fw.write(docno+"\t");
                 int doclen = 0;
-                TermsEnum termsEnum = index.getTermVector( docid, field_search ).iterator();
-                while ( termsEnum.next() != null ) {
-                    doclen += termsEnum.totalTermFreq();
-                }
+                /* if the index has positions, then uncomment certain lines */
+
+//                TermsEnum termsEnum = index.getTermVector( docid, field_search ).iterator();
+//                while ( termsEnum.next() != null ) {
+//                    doclen += termsEnum.totalTermFreq();
+//                }
 
 //                String [] termarr = new String[doclen];
-                ArrayList<String> termslist = new ArrayList<>();
+//                ArrayList<String> termslist = new ArrayList<>();
                 Terms vector = index.getTermVector(docid, field_search); // Read the document's document vector.
                 TermsEnum iterator = vector.iterator();
                 BytesRef term;
-                PostingsEnum positions = null;
-                long len = 0;
+//                PostingsEnum positions = null;
                 while ((term = iterator.next()) != null) {
                     long freq = iterator.totalTermFreq();
                     String termstr = term.utf8ToString();
@@ -49,17 +55,17 @@ public class makeCorpus {
 //                        int p = positions.nextPosition();
 //                        System.out.println(p);
 //                        termarr[p] = termstr;
-                        termslist.add(termstr);
-//                        len++;
+//                        termslist.add(termstr);
+                        fw.write(termstr+" ");
                     }
                 }
-                System.out.println(len+" "+doclen);
-                System.out.println(termslist);
-                if (docid > 10){
-                    break;
-                }
+                fw.write("\n");
+//                if (docid > 10){
+//                    break;
+//                }
 
             }
+            fw.close();
             index.close();
             dirLucene.close();
         } catch (IOException e) {
--- explainable-results/src/python/predict_proba.py
+++ explainable-results/src/python/predict_proba.py
@@ -1,27 +1,30 @@
+import sys
+import lime
+
 class Pred:
     def __init__(self):
         self.corpus = None
-        self.topscore = -(sys.maxint - 5) 
-        self.kthscore = -(sys.maxint - 5)
+        self.topscore = -(sys.maxsize - 5)
+        self.kthscore = -(sys.maxsize - 5)
         self.kthrank = -1
 
     def _make_tfidfvecs(self):
         # vectorizer = TfidfVectorizer()
         # X_sparse = vectorizer.fit_transform(self.corpus)
         # X_mat = X_sparse.toarray()
+        pass
 
-         
-    def _get_prob(self, score, rank=0, type="score"):
+    def _get_prob(self, score=0.0, rank=0, type="score", k=1):
+
         if type == "score":
             return 1 - ((self.topscore - score) / self.topscore)
         elif type == "topk":
-            return 1 if self.kthscore <= score else 0 
+            return 1 if self.kthscore <= score else 0
         elif type == "rank":
-            return 1 - (1.0*rank) / k if self.kthrank >= rank else 0 
+            return 1 - (1.0 * rank) / k if self.kthrank >= rank else 0
 
-    
     def read_file(self, filename):
-        k = 10 
+        k = 10
         namelist = []
         textlist = []
         scorelist = []
@@ -31,24 +34,26 @@ class Pred:
                 namelist.append(splitline[0])
                 textlist.append(splitline[1])
                 scorelist.append(splitline[2])
-        
+
         self.topscore = scorelist[0]
         self.kthscore = scorelist[k]
         self.corpus = list(zip([namelist, textlist, scorelist]))
         self.kthrank = k
         return
 
-    def predict_proba(instance):
+    def predict_proba(self):
         return
 
-    def lime_func():
+    def lime_func(self):
         return
+
 
 def main():
     filename = "/Users/dhruva/Desktop/ISR/final_project/explainable-results/file_text.txt"
     p = Pred()
     p.read_file(filename)
-    p.predict_proba(l[3])
+
+    p.predict_proba()
     return
 
 

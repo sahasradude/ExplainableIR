215b4e678b8f4b10f7a35a85a5b5e4d08b72f8bd
sahasradude <dhruvas@vt.edu>
2018-11-27 19:05:29 -0500

    changes in predict_prob

--- explainable-results/src/python/predict_proba.py
+++ explainable-results/src/python/predict_proba.py
@@ -1,5 +1,6 @@
 import sys
 import lime
+from lime.lime_text import LimeTextExplainer
 
 
 # load a couple of models, run lime on both for the top instances, and then see what the explanations are for those instances
@@ -10,15 +11,10 @@ import lime
 class Pred:
     def __init__(self):
         self.corpus = None
+        self.query = ""
         self.topscore = -(sys.maxsize - 5)
         self.kthscore = -(sys.maxsize - 5)
         self.kthrank = -1
-
-    def _make_tfidfvecs(self):
-        # vectorizer = TfidfVectorizer()
-        # X_sparse = vectorizer.fit_transform(self.corpus)
-        # X_mat = X_sparse.toarray()
-        pass
 
     def _get_prob(self, score=0.0, rank=0, type="score", k=1):
 
@@ -47,7 +43,7 @@ class Pred:
         self.kthrank = k
         return
 
-    def read_mapped_file():
+    def read_mapped_file(self):
         a = dict()
         with open("relation.txt", "r+") as f:
             for line in f.readlines():
@@ -64,24 +60,23 @@ class Pred:
 
         for query in a:
             for doc in a[query]:
+                self.query = query
                 document_text = corpus[doc]
-                lime(document_text, predict_proba(doc, query), )
+                explainer = LimeTextExplainer(class_names=["relevant", "irrelevant"])
+                explainer.explain_instance(document_text, self.predict_proba, 10)
 
-            
 
-        self.topscore = scorelist[0]
-        self.kthscore = scorelist[k]
-        self.corpus = list(zip([namelist, textlist, scorelist]))
-        self.kthrank = k
+
         return
 
-    def predict_proba(doc, query):
+    def predict_proba(self, doc_text):
+        doc = self.find_docno(doc_text)
         with open("predict.test.drmm.Testing.txt", "r+") as f:
             for line in f.readlines():
                 splitline = line.split(" ")
-                if splitline[0] == query and split[2] == doc:
-                    return (splitline[3],splitline[4])
-        return (-1,-1)
+                if splitline[0] == query and splitline[2] == doc:
+                    return self._get_prob(rank=splitline[3],score=splitline[4])
+        return 0
 
     def lime_func(self):
         return
